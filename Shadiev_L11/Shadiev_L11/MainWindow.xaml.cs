d047d127531c0d63ab334d0b2a9e18e0404ba5cf
TheLogikF <150513485+TheLogikF@users.noreply.github.com>
2024-12-09 13:13:39 +0500

    Add files via upload

---
+++ Shadiev_L11/Shadiev_L11/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Timers;
+
+namespace Shadiev_L11
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DateTime? DT; //Task3
+        public MainWindow()
+        {
+            InitializeComponent();
+            Calendar2.BlackoutDates.Clear();
+        }
+        #region Task1
+        private void Calendar1_SelectedDatesChanged(object sender, SelectionChangedEventArgs e) => MessageBox.Show(Calendar1.SelectedDate.Value.Date.ToShortDateString());
+        #endregion
+        #region Task3
+        private void Calendar2_SelectedDatesChanged(object sender, SelectionChangedEventArgs e) => DT = Calendar2.SelectedDate;
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                TimeSpan time = (TimeSpan)(DT - DateTime.Now);
+                MessageBox.Show($"{time.Days}/{time.Hours}:{time.Minutes}:{time.Seconds}");
+            }
+            catch
+            {
+                MessageBox.Show("Ну это\nНу типо\nТи вибрал этот же день");
+            }
+        }
+        #endregion
+    }
+}
---
+++ Shadiev_L12/Shadiev_L12/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Diagnostics;
+using System.IO;
+
+namespace Shadiev_L12
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Task3
+        List<BitmapImage> Images = new List<BitmapImage>();
+        int currentImage = 0;
+        #endregion
+        public MainWindow()
+        {
+            InitializeComponent();
+            #region Task3
+            Images.Add(new BitmapImage(new Uri(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Bear1.png"))));
+            Images.Add(new BitmapImage(new Uri(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Bear2.png"))));
+            Images.Add(new BitmapImage(new Uri(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Bear3.png"))));
+            Images.Add(new BitmapImage(new Uri(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Bear4.png"))));
+            Images.Add(new BitmapImage(new Uri(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Bear5.png"))));
+            Images.Add(new BitmapImage(new Uri(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Bear6.png"))));
+            Images.Add(new BitmapImage(new Uri(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Bear7.png"))));
+            Images.Add(new BitmapImage(new Uri(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Bear8.png"))));
+            Images.Add(new BitmapImage(new Uri(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Bear9.png"))));
+            Images.Add(new BitmapImage(new Uri(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Bear10.png"))));
+            Photo.Source = Images[currentImage];
+            #endregion
+        }
+        #region Task3
+        private void NextPhoto_Click(object sender, RoutedEventArgs e)
+        {
+            if (currentImage < Images.Count - 1) currentImage++;
+            if (currentImage == 1) LastPhoto.Visibility = Visibility.Visible;
+            if (currentImage == Images.Count - 1) NextPhoto.Visibility = Visibility.Collapsed;
+            Photo.Source = Images[currentImage];
+        }
+        private void LastPhoto_Click(object sender, RoutedEventArgs e)
+        {
+            if (currentImage > 0) currentImage--;
+            if (currentImage == 0) LastPhoto.Visibility = Visibility.Collapsed;
+            if (currentImage == Images.Count - 2) NextPhoto.Visibility = Visibility.Visible;
+            Photo.Source = Images[currentImage];
+        }
+        #endregion
+        #region Task4
+        private void StackPanel_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.OriginalSource is Image image)
+                CurrentImage.Source = image.Source;
+        }
+        #endregion
+    }
+}
